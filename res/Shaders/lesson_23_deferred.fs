8ab70d77736853f0719c87753e156fc2230d9717
FDavis348915654 <348915654@qq.com>
2022-11-21 00:08:37 +0800

    延迟着色法。着色器提交

---
+++ res/Shaders/lesson_23_deferred.fs
@@ -0,0 +1,46 @@
+#version 330 core
+
+// 延迟着色法
+
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D gPosition;
+uniform sampler2D gNormal;
+uniform sampler2D gAlbedoSpec;
+
+struct Light {
+    vec3 Postion;
+    vec3 Color;
+
+    float Linear;
+    float Quadratic; // 半径距离的2次方
+};
+
+const int NR_LIGHTS = 32;
+uniform Light lights[NR_LIGHTS];
+uniform vec3 viewPos;
+
+void main() {
+    vec3 FragPos = texture(gPosition, TexCoords).rgb;
+    vec3 Normal = texture(gNormal, TexCoords).rgb;
+    vec3 Diffuse = texture(gAlbedoSpec, TexCoords).rgb;
+    float Specular = texture(gAlbedoSpec, TexCoords).a;
+
+    vec3 lighting = Diffuse * 0.1;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    for (int i = 0; i < NR_LIGHTS; i++) {
+        vec3 lightDir = normalize(lights[i].Postion - FragPos);
+        vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Diffuse * lights[i].Color;
+        vec3 halfwayDir = normalize(lightDir + viewDir);
+        float spec = pow(max(dot(Normal, halfwayDir), 0.0), 16.0);
+        vec3 specular = lights[i].Color * spec * Specular;
+        float distance = length(lights[i].Postion - FragPos);
+        float attenuation = 1.0 / (1.0 + lights[i].Linear * distance + lights[i].Quadratic * distance * distance);
+        diffuse *= attenuation;
+        specular *= attenuation;
+        lighting += diffuse * specular;
+    }
+    FragColor = vec4(lighting, 1.0);
+}
---
+++ res/Shaders/lesson_23_deferred_g_buffer.fs
@@ -0,0 +1,26 @@
+#version 330 core
+
+// 延迟着色法
+
+// MRT(MultipleRenderTargets)
+layout (location = 0) out vec3 gPosition; // 顶点坐标
+layout (location = 1) out vec3 gNormal; // 法线坐标
+layout (location = 2) out vec4 gAlbedoSpec; // 漫反射颜色+镜面高光
+
+in vec3 FragPos;
+in vec2 TexCoords;
+in vec3 Normal;
+
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
+
+void main() {
+    // 保存顶点坐标
+    gPosition = FragPos;
+    // 法线坐标
+    gNormal = normalize(Normal);
+    // 漫反射颜色
+    gAlbedoSpec.rgb = = texture(texture_diffuse1, TexCoords).rgb;
+    // 镜面高光
+    gAlbedoSpec.a = texture(texture_specular1, TexCoords).r;
+}
